0c5de341f10f930149c12e6cf33dcf4c2aab9b82
Litpe2point0 <bachdxse182030@fpt.edu.vn>
2024-07-25 18:09:23 +0700

    li

--- Bijoux_Jewelry/Bijoux_Jewelry.DataAccess/obj/Debug/net8.0/Bijoux_Jewelry.DataAccess.AssemblyInfo.cs
+++ Bijoux_Jewelry/Bijoux_Jewelry.DataAccess/obj/Debug/net8.0/Bijoux_Jewelry.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bijoux_Jewelry.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e452aa0b40168d39255e41e85818799468965012")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b705c1d7fc11102b32852b04b49525b14fb7f273")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bijoux_Jewelry.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bijoux_Jewelry.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
